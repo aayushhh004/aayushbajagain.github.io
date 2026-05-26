f5ef68206b2edcfd2f2c5de2cc2da21009a26edd
aayushhh004 <bajagainaayush42@gmail.com>
2026-05-26 20:29:10 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+particlesJS("particles-js", {
+
+  particles: {
+
+    number: {
+      value: 70
+    },
+
+    size: {
+      value: 3
+    },
+
+    move: {
+      speed: 2
+    },
+
+    line_linked: {
+      enable: true,
+      opacity: 0.2
+    },
+
+    color: {
+      value: "#ffffff"
+    }
+
+  }
+
+});
+
+AOS.init();
